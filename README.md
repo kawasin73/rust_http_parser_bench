73769e21b6301539b828f339ef3c27244fcffa7e
Kawamura Shintaro <kawasin73@gmail.com>
2023-01-09 11:25:47 +0900

    add benchmarks

--- README.md
+++ README.md
@@ -1,5 +1,78 @@
 # rust http parser bench
 
+The fastest HTTP parser of Rust is `picohttpparser_sys` which is Rust binding of
+C library though.
+
+The fastest pure Rust HTTP parser is `thhp`.
+
+```
+$ cargo +nightly bench
+   Compiling rust_http_parser_bench v0.1.0 (/Users/kawasin73/src/github.com/kawasin73/rust_http_parser_bench)
+    Finished bench [optimized] target(s) in 1.68s
+warning: the following packages contain code that will be rejected by a future version of Rust: traitobject v0.1.0
+note: to see what the problems were, use the option `--future-incompat-report`, or run `cargo report future-incompatibilities --id 76`
+     Running unittests src/main.rs (target/release/deps/rust_http_parser_bench-63366b14855362d6)
+
+running 16 tests
+test tests::bench_dumb_http_parser   ... bench:       2,012 ns/iter (+/- 174) = 349 MB/s
+test tests::bench_http_box           ... bench:         645 ns/iter (+/- 73) = 1089 MB/s
+test tests::bench_http_bytes         ... bench:       2,288 ns/iter (+/- 311) = 307 MB/s
+test tests::bench_http_muncher       ... bench:         876 ns/iter (+/- 100) = 802 MB/s
+test tests::bench_http_parser        ... bench:       3,889 ns/iter (+/- 378) = 180 MB/s
+test tests::bench_http_pull_parser   ... bench:       7,616 ns/iter (+/- 855) = 92 MB/s
+test tests::bench_http_tiny          ... bench:      14,301 ns/iter (+/- 1,454) = 49 MB/s
+test tests::bench_httparse           ... bench:         298 ns/iter (+/- 36) = 2359 MB/s
+test tests::bench_milstian_http      ... bench:      25,205 ns/iter (+/- 2,708) = 27 MB/s
+test tests::bench_picohttpparser_sys ... bench:         143 ns/iter (+/- 11) = 4916 MB/s
+test tests::bench_rhymuweb           ... bench:       6,789 ns/iter (+/- 1,055) = 103 MB/s
+test tests::bench_rocket_http_hyper  ... bench:       4,255 ns/iter (+/- 527) = 165 MB/s
+test tests::bench_saf_httparser      ... bench:       6,293 ns/iter (+/- 784) = 111 MB/s
+test tests::bench_stream_httparse    ... bench:       1,947 ns/iter (+/- 194) = 361 MB/s
+test tests::bench_thhp               ... bench:         237 ns/iter (+/- 34) = 2966 MB/s
+test tests::bench_uhttp_request      ... bench:         274 ns/iter (+/- 31) = 2565 MB/s
+
+test result: ok. 0 passed; 0 failed; 0 ignored; 16 measured; 0 filtered out; finished in 48.00s
+```
+
+```
+$ RUSTFLAGS="-C target-feature=+sse4.2" cargo +nightly bench
+    Finished bench [optimized] target(s) in 0.28s
+warning: the following packages contain code that will be rejected by a future version of Rust: traitobject v0.1.0
+note: to see what the problems were, use the option `--future-incompat-report`, or run `cargo report future-incompatibilities --id 78`
+     Running unittests src/main.rs (target/release/deps/rust_http_parser_bench-63366b14855362d6)
+
+running 16 tests
+test tests::bench_dumb_http_parser   ... bench:       2,085 ns/iter (+/- 537) = 337 MB/s
+test tests::bench_http_box           ... bench:         592 ns/iter (+/- 43) = 1187 MB/s
+test tests::bench_http_bytes         ... bench:       2,347 ns/iter (+/- 341) = 299 MB/s
+test tests::bench_http_muncher       ... bench:         795 ns/iter (+/- 141) = 884 MB/s
+test tests::bench_http_parser        ... bench:       3,791 ns/iter (+/- 594) = 185 MB/s
+test tests::bench_http_pull_parser   ... bench:       6,990 ns/iter (+/- 658) = 100 MB/s
+test tests::bench_http_tiny          ... bench:      12,675 ns/iter (+/- 1,219) = 55 MB/s
+test tests::bench_httparse           ... bench:         276 ns/iter (+/- 11) = 2547 MB/s
+test tests::bench_milstian_http      ... bench:      23,279 ns/iter (+/- 2,616) = 30 MB/s
+test tests::bench_picohttpparser_sys ... bench:         135 ns/iter (+/- 24) = 5207 MB/s
+test tests::bench_rhymuweb           ... bench:       6,868 ns/iter (+/- 631) = 102 MB/s
+test tests::bench_rocket_http_hyper  ... bench:       4,119 ns/iter (+/- 420) = 170 MB/s
+test tests::bench_saf_httparser      ... bench:       5,870 ns/iter (+/- 528) = 119 MB/s
+test tests::bench_stream_httparse    ... bench:       1,781 ns/iter (+/- 202) = 394 MB/s
+test tests::bench_thhp               ... bench:         177 ns/iter (+/- 14) = 3971 MB/s
+test tests::bench_uhttp_request      ... bench:         261 ns/iter (+/- 24) = 2693 MB/s
+
+test result: ok. 0 passed; 0 failed; 0 ignored; 16 measured; 0 filtered out; finished in 19.03s
+```
+
+## Environment
+
+* Macbook Pro (13-inch, 2018, Four Thunderbolt 3 Ports)
+* Processor: 2.7 GHz Quad-Core Intel Core i7
+* Memory: 16 GB 2133 MHz LPDDR3
+
+```
+$ cargo -V
+cargo 1.68.0-nightly (8c460b223 2023-01-04)
+```
+
 ## LICENSE
 
 MIT
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,337 @@
-fn main() {
-    println!("Hello, world!");
+#![feature(test)]
+
+extern crate test;
+
+fn main() {}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    use std::{ptr, io::Cursor};
+
+    use test::Bencher;
+
+    /// benchmark HTTP request from https://github.com/h2o/picohttpparser/blob/master/bench.c
+    const REQUEST: &[u8] =
+        b"GET /wp-content/uploads/2010/03/hello-kitty-darth-vader-pink.jpg HTTP/1.1\r\n\
+        Host: www.kittyhell.com\r\n\
+        User-Agent: Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.6; ja-JP-mac; rv:1.9.2.3) Gecko/20100401 Firefox/3.6.3 \
+        Pathtraq/0.9\r\n\
+        Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r\n\
+        Accept-Language: ja,en-us;q=0.7,en;q=0.3\r\n\
+        Accept-Encoding: gzip,deflate\r\n\
+        Accept-Charset: Shift_JIS,utf-8;q=0.7,*;q=0.7\r\n\
+        Keep-Alive: 115\r\n\
+        Connection: keep-alive\r\n\
+        Cookie: wp_ozh_wsa_visits=2; wp_ozh_wsa_visit_lasttime=xxxxxxxxxx; \
+        __utma=xxxxxxxxx.xxxxxxxxxx.xxxxxxxxxx.xxxxxxxxxx.xxxxxxxxxx.x; \
+        __utmz=xxxxxxxxx.xxxxxxxxxx.x.x.utmccn=(referral)|utmcsr=reader.livedoor.com|utmcct=/reader/|utmcmd=referral\r\n\
+        \r\n";
+
+    struct Callback;
+
+    #[bench]
+    fn bench_http_parser(b: &mut Bencher) {
+        use http_parser::HttpParser;
+        use http_parser::HttpParserType;
+        use http_parser::HttpParserCallback;
+
+        impl HttpParserCallback for Callback {
+        }
+
+        let mut cb = Callback{};
+
+        b.iter(|| {
+            let mut parser = HttpParser::new(HttpParserType::Request);
+            let size = parser.execute(&mut cb, REQUEST);
+            assert_eq!(size, REQUEST.len());
+        });
+        b.bytes = REQUEST.len() as u64;
+    }
+
+    #[bench]
+    fn bench_dumb_http_parser(b: &mut Bencher) {
+        use dumb_http_parser::HttpParser;
+
+        b.iter(|| {
+            let mut parser = HttpParser::new(REQUEST);
+            parser.parse();
+        });
+        b.bytes = REQUEST.len() as u64;
+    }
+
+    #[bench]
+    fn bench_http_box(b: &mut Bencher) {
+        use http_box::http1::Parser;
+        use http_box::fsm::Success;
+        use http_box::http1::HttpHandler;
+        impl HttpHandler for Callback {
+        }
+
+        let mut parser = Parser::new();
+        let mut cb = Callback{};
+
+        b.iter(|| {
+            match parser.resume(&mut cb, REQUEST).unwrap() {
+                Success::Finished(size) => {
+                    assert_eq!(size, REQUEST.len());
+                },
+                other => {
+                    panic!("unexpected: {:?}", other);
+                }
+            };
+            parser.reset();
+        });
+        b.bytes = REQUEST.len() as u64;
+    }
+
+    #[bench]
+    fn bench_thhp(b: &mut Bencher) {
+        use thhp::Request;
+        use thhp::Status;
+
+        let mut headers = Vec::<thhp::HeaderField>::with_capacity(32);
+        b.iter(|| {
+            headers.clear();
+            match Request::parse(REQUEST, &mut headers).unwrap() {
+                Status::Complete((_, size)) => {
+                    assert_eq!(size, REQUEST.len());
+                },
+                other => {
+                    panic!("unexpected: {:?}", other);
+                }
+            }
+        });
+        b.bytes = REQUEST.len() as u64;
+    }
+
+    #[bench]
+    fn bench_stream_httparse(b: &mut Bencher) {
+        use stream_httparse::streaming_parser::ReqParser;
+
+        let mut parser = ReqParser::new_capacity(32);
+        b.iter(|| {
+            match parser.block_parse(REQUEST) {
+                (true, None) => {},
+                other => {
+                    panic!("unexpected: {:?}", other);
+                }
+            }
+            parser.finish().unwrap();
+            parser.clear();
+        });
+        b.bytes = REQUEST.len() as u64;
+    }
+
+    #[bench]
+    fn bench_http_pull_parser(b: &mut Bencher) {
+        use http_pull_parser::Parser;
+
+        b.iter(|| {
+            let mut parser = Parser::request();
+            let mut nsize = 0;
+            while nsize < REQUEST.len() {
+                let (_, size) = parser.next_token(Some(&REQUEST[nsize..]));
+                nsize += size;
+            }
+        });
+        b.bytes = REQUEST.len() as u64;
+    }
+
+    #[bench]
+    fn bench_picohttpparser_sys(b: &mut Bencher) {
+        use picohttpparser_sys::phr_parse_request;
+        use picohttpparser_sys::phr_header;
+
+        let mut method: *const libc::c_char = ptr::null();
+        let mut method_len: usize = 0;
+        let mut path: *const libc::c_char = ptr::null();
+        let mut path_len: usize = 0;
+        let mut minor_version: libc::c_int  = 0;
+        let mut headers = [phr_header::default();32];
+        let mut num_headers: usize = 32;
+        b.iter(|| {
+            num_headers = 32;
+            let size = unsafe {
+                phr_parse_request(
+                    REQUEST.as_ptr() as *const libc::c_char,
+                    REQUEST.len(),
+                    &mut method,
+                    &mut method_len,
+                    &mut path,
+                    &mut path_len,
+                    &mut minor_version,
+                    headers.as_mut_ptr(),
+                    &mut num_headers, 0) as usize
+            };
+            assert_eq!(size, REQUEST.len());
+        });
+        b.bytes = REQUEST.len() as u64;
+    }
+
+    #[bench]
+    fn bench_http_bytes(b: &mut Bencher) {
+        use http_bytes::parse_request_header;
+        let mut headers_buffer = vec![http_bytes::EMPTY_HEADER; 32];
+
+        b.iter(|| {
+            let (_, rest) = parse_request_header(REQUEST, &mut headers_buffer, Some(http_bytes::http::uri::Scheme::HTTP)).unwrap().unwrap();
+            assert_eq!(rest.len(), 0);
+        });
+        b.bytes = REQUEST.len() as u64;
+    }
+
+    #[bench]
+    fn bench_httparse(b: &mut Bencher) {
+        use httparse::Request;
+        use httparse::Status;
+
+        let mut headers = [httparse::EMPTY_HEADER; 16];
+
+        b.iter(|| {
+            let mut req = Request::new(&mut headers);
+            let status = req.parse(REQUEST).unwrap();
+            match status {
+                Status::Complete(size) => {
+                    assert_eq!(size, REQUEST.len());
+                },
+                other => {
+                    panic!("unexpected: {:?}", other);
+                }
+            }
+        });
+        b.bytes = REQUEST.len() as u64;
+    }
+
+    #[bench]
+    fn bench_rhymuweb(b: &mut Bencher) {
+        use rhymuweb::Request;
+
+        b.iter(|| {
+            let mut req = Request::new();
+            let result = req.parse(REQUEST).unwrap();
+            assert_eq!(result.consumed, REQUEST.len());
+        });
+        b.bytes = REQUEST.len() as u64;
+    }
+
+    #[bench]
+    fn bench_saf_httparser(b: &mut Bencher) {
+        use saf_httparser::request_from_bytes;
+
+        b.iter(|| {
+            let _ = request_from_bytes(REQUEST).unwrap();
+        });
+        b.bytes = REQUEST.len() as u64;
+    }
+
+    #[bench]
+    fn bench_http_muncher(b: &mut Bencher) {
+        use http_muncher::Parser;
+        use http_muncher::ParserHandler;
+        impl ParserHandler for Callback {}
+
+        let mut cb = Callback{};
+
+        b.iter(|| {
+            let mut parser = Parser::request();
+            let size = parser.parse(&mut cb, REQUEST);
+            assert_eq!(size, REQUEST.len());
+        });
+        b.bytes = REQUEST.len() as u64;
+    }
+
+    #[bench]
+    fn bench_milstian_http(b: &mut Bencher) {
+        use milstian_http::request::Message;
+
+        b.iter(|| {
+            let msg = Message::from_tcp_stream(REQUEST);
+            assert!(msg.is_some());
+        });
+        b.bytes = REQUEST.len() as u64;
+    }
+
+    #[bench]
+    fn bench_rocket_http_hyper(b: &mut Bencher) {
+        use rocket_http::hyper::h1::parse_request;
+        use rocket_http::hyper::buffer::BufReader;
+
+        b.iter(|| {
+            let mut buf = BufReader::new(Cursor::new(REQUEST));
+            let _ = parse_request(&mut buf).unwrap();
+        });
+        b.bytes = REQUEST.len() as u64;
+    }
+
+    #[bench]
+    fn bench_http_tiny(b: &mut Bencher) {
+        use http_tiny::HeaderStartLine;
+        use http_tiny::HeaderFields;
+
+        b.iter(|| {
+            let mut buf = Cursor::new(REQUEST);
+            let _ = HeaderStartLine::read(&mut buf).unwrap();
+            let _ = HeaderFields::read(&mut buf).unwrap();
+            assert_eq!(buf.position() as usize, REQUEST.len());
+        });
+        b.bytes = REQUEST.len() as u64;
+    }
+
+    // #[bench]
+    // #[ignore = "pico is broken"]
+    // fn bench_pico(b: &mut Bencher) {
+    //     use pico::request::RequestParser;
+    //     use pico::HEADER_EMPTY;
+    //     use pico::ChunkReader;
+    //     use pico::Chunks;
+
+    //     struct RequestReader {
+    //         read: usize,
+    //     }
+
+    //     impl ChunkReader<RequestReader> for RequestReader {
+    //         fn read(self, buf: &mut [u8]) -> (Option<usize>, RequestReader) {
+    //             let size = std::cmp::min(REQUEST.len() - self.read, buf.len());
+    //             std::ptr::copy_nonoverlapping(REQUEST[self.read..].as_ptr(), buf.as_mut_ptr(), size);
+    //             self.read += size;
+    //             (Some(size), self)
+    //         }
+    //     }
+
+    //     impl Chunks for RequestReader {
+    //         type Reader: ChunkReader<RequestReader>;
+
+    //         fn chunk<F>(self, f: F) where F: FnOnce(Self::Reader) {
+    //             f(self);
+    //         }
+    //     }
+
+
+    //     let mut buf = [0_u8; 4096];
+    //     let mut headers = [HEADER_EMPTY; 32];
+
+    //     b.iter(|| {
+    //         let parser = RequestParser::new(&mut buf, &mut headers);
+    //         parser.parse(Reader{read: 0}, |result, _, body| {
+    //             let _ = result.unwrap();
+    //             assert_eq!(body.len(), 0);
+    //         })
+    //     });
+    // }
+
+    #[bench]
+    fn bench_uhttp_request(b: &mut Bencher) {
+        use uhttp_request::RequestLine;
+        use uhttp_request::Headers;
+
+        b.iter(|| {
+            let (_, rest) = RequestLine::new(REQUEST).unwrap();
+            let mut headers = Headers::new(rest);
+            while let Some(_) = headers.next() {}
+            assert_eq!(headers.into_inner().len(), 0);
+        });
+        b.bytes = REQUEST.len() as u64;
+    }
 }
